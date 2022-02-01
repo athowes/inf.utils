57e7d09d1a08c4dd35f326e41e3c3600390c2731
Adam Howes <adamhowes@outlook.com>
2022-02-01 15:34:18 +0000

    Oops!

--- R/aghq-summary.R
+++ R/aghq-summary.R
@@ -1,11 +1,11 @@
 aghq_summary <- function(quad) {
-  aghq_hyper <- aghq::summary(quad)$aghqsummary$summarytable %>%
+  aghq_hyper <- summary(quad)$aghqsummary$summarytable %>%
     as.data.frame() %>%
     tibble::rownames_to_column() %>%
     dplyr::rename(parameter = rowname) %>%
     dplyr::select(parameter, mean, sd)
 
-  aghq_random <- aghq::summary(quad)$randomeffectsummary %>%
+  aghq_random <- summary(quad)$randomeffectsummary %>%
     as.data.frame() %>%
     tibble::rownames_to_column() %>%
     # Warning: this rownumber as index only works when you have one random effect!
