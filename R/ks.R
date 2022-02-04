63d3eef60a24e5c8697d41d21bd910bd7cd81ac9
Adam Howes <adamhowes@outlook.com>
2022-02-04 16:01:34 +0000

    Replace ks.test with a custom one which also includes the location where D occurs (location in input space where there is the maximum ECDF difference)

--- R/ks.R
+++ R/ks.R
@@ -1,4 +1,9 @@
-get_ks <- function(x, y) {
-  suppressWarnings(capture.output(ks <- ks.test(x, y)))
-  as.numeric(unname(ks$statistic))
+ks_test <- function(x, y) {
+  nx <- length(x)
+  ny <- length(y)
+  w <- c(x, y)
+  o <- order(w)
+  z <- cumsum(ifelse(o <= nx, 1 / nx, -1 / ny))
+  i <- which.max(abs(z))
+  return(list("D" = max(abs(z)), "l" = w[o[i]]))
 }
