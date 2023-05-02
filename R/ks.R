bd3884d586edd30171a7af2a10b79352dcdaead5
athowes <adamthowes@gmail.com>
2023-05-02 15:09:24 +0100

    Update ks_test to work in all equal case

--- R/ks.R
+++ R/ks.R
@@ -2,6 +2,9 @@ ks_test <- function(x, y) {
   nx <- length(x)
   ny <- length(y)
   w <- c(x, y)
+  if(length(unique(w)) == 1) {
+    return(list("D" = 0, "l" = w[1]))
+  }
   o <- order(w)
   z <- cumsum(ifelse(o <= nx, 1 / nx, -1 / ny))
   i <- which.max(abs(z))
